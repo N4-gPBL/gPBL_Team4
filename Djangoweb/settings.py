db31ad9334a9725bb20a4dcb008759beb8cea3fd
vnkylin266 <tuanta@ql6625.fr>
2022-11-09 10:46:32 +0700

    remove environ

--- Djangoweb/settings.py
+++ Djangoweb/settings.py
@@ -13,14 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 
-import environ
-
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
