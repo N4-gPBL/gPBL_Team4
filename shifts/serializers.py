cf89e0f906ebbe5d2c2d7e1c0c0287f00b3475f2
vnkylin266 <tuanta@ql6625.fr>
2022-11-16 16:00:16 +0700

    add function shift

--- shifts/serializers.py
+++ shifts/serializers.py
@@ -1,22 +1,32 @@
 from .models import Shift,UserShift
-
+from rest_framework import serializers
 class ShiftSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shift
-        fields = ('id','shift_start', 'shift_end','shift_date', 'shift_status', 'shift_created_at', 'shift_updated_at')
+        fields = ('shift_start', 'shift_end','shift_date', 'shift_status', 'shift_created_at', 'shift_updated_at')
     
     def __init__(self, instance=None, data=..., **kwargs):
         super().__init__(instance, data, **kwargs)
-        self.fields['id'].read_only = True
         self.fields['shift_start'].required = True
         self.fields['shift_end'].required = True
         self.fields['shift_date'].required = True
         self.fields['shift_status'].required = False
         self.fields['shift_created_at'].required = False
         self.fields['shift_updated_at'].required = False
+    def create(self, validated_data):
+        shift = Shift.objects.create(**validated_data)
+        return shift
+    def update(self, instance, validated_data):
+        instance.shift_start = validated_data.get('shift_start', instance.shift_start)
+        instance.shift_end = validated_data.get('shift_end', instance.shift_end)
+        instance.shift_date = validated_data.get('shift_date', instance.shift_date)
+        instance.shift_status = validated_data.get('shift_status', instance.shift_status)
+        instance.shift_created_at = validated_data.get('shift_created_at', instance.shift_created_at)
+        instance.shift_updated_at = validated_data.get('shift_updated_at', instance.shift_updated_at)
+        instance.save()
+        return instance
 
-
-
+ 
 class UserShiftSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserShift
@@ -28,4 +38,3 @@ class UserShiftSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user_shift = UserShift.objects.create(**validated_data)
         return user_shift
-    
--- shifts/views.py
+++ shifts/views.py
@@ -1,39 +1,65 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-
+from .serializers import ShiftSerializer, UserShiftSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Shift,UserShift
 # Create your views here.
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def createShift(request):
-    pass
+    serializer = ShiftSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getShift(request, shift_id):
-    pass
+    shift = Shift.objects.get(shift_id=shift_id)
+    serializer = ShiftSerializer(shift, many=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getAllShifts(request):
-    pass
+    shifts = Shift.objects.all()
+    serializer = ShiftSerializer(shifts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def updateShift(request, shift_id):
-    pass
+    shift = Shift.objects.get(shift_id=shift_id)
+    serializer = ShiftSerializer(instance=shift, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
 def deleteShift(request, shift_id):
-    pass
+    shift = Shift.objects.get(shift_id=shift_id)
+    shift.delete()
+    return Response('Shift successfully deleted!', status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def assignShift(request, shift_id):
-    pass
+    shift = Shift.objects.get(shift_id=shift_id)
+    serializer = UserShiftSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def unassignShift(request, shift_id):
-    pass
+    shift = Shift.objects.get(shift_id=shift_id)
+    user_shift = UserShift.objects.get(user_shift_shift_id=shift)
+    user_shift.delete()
+    return Response('Item successfully deleted!', status=status.HTTP_200_OK)
